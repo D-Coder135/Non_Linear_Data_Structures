a2eec41648ac8bedeb7b43e1922ceae9140b691c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-17 19:26:28 +0530

    Add a class BinarySearchTree.java

---
+++ src/Tree/BinarySearchTree/implementation/BinarySearchTree.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 17/04/21
+ *   Time: 7:25 PM
+ *   File: BinarySearchTree.java
+ */
+
+package Tree.BinarySearchTree.implementation;
+
+public class BinarySearchTree {
+}
