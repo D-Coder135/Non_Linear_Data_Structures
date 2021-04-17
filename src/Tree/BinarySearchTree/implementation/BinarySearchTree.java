8cf0767516b7deccf967680d0cc79d0c19fff09e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-18 00:41:01 +0530

    Add statements to the body of the if() block

--- src/Tree/BinarySearchTree/implementation/BinarySearchTree.java
+++ src/Tree/BinarySearchTree/implementation/BinarySearchTree.java
@@ -46,7 +46,9 @@ public class BinarySearchTree<E extends Comparable<E>> {
 
     public void inOrderTraversal(Node<E> node) {
         if (node != null) {
-
+            inOrderTraversal(node.getLeft());
+            System.out.println(node.getData() + ", ");
+            inOrderTraversal(node.getRight());
         }
     }
 }
