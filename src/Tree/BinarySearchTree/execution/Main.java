1d49b249028f0c5b4336cbaa8e2c6d703dbc4e3e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-18 13:42:43 +0530

    Add a class Main.java inside the package execution

---
+++ src/Tree/BinarySearchTree/execution/Main.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 18/04/21
+ *   Time: 1:42 PM
+ *   File: Main.java
+ */
+
+package Tree.BinarySearchTree.execution;
+
+public class Main {
+}
