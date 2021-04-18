6566bb30e5797fb0f03d69d3272e22cb962c7ccd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-18 13:25:43 +0530

    Add a if() block with a condition to it

--- src/Tree/BinarySearchTree/implementation/BinarySearchTree.java
+++ src/Tree/BinarySearchTree/implementation/BinarySearchTree.java
@@ -136,6 +136,9 @@ public class BinarySearchTree<E extends Comparable<E>> {
 
     private boolean hasLeftSubTree(Node<E> temp) {
         boolean response = false;
+        if (temp.getLeft() != null && temp.getRight() == null) {
+
+        }
     }
 
     private boolean isLeafNode(Node<E> temp) {
