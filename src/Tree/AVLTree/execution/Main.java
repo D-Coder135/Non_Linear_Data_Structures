53a92dd56314d15c4fd2bf346aa4e1ad71dfcd0e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-22 18:38:32 +0530

    Add a class Main.java inside the package AVLTree.execution

---
+++ src/Tree/AVLTree/execution/Main.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 22/04/21
+ *   Time: 6:38 PM
+ *   File: Main.java
+ */
+
+package Tree.AVLTree.execution;
+
+public class Main {
+}
