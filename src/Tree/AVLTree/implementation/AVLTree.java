810da8202290398255736e103b030677b41f347a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-22 10:41:37 +0530

    Add statement to the body of the if() block

--- src/Tree/AVLTree/implementation/AVLTree.java
+++ src/Tree/AVLTree/implementation/AVLTree.java
@@ -20,7 +20,7 @@ public class AVLTree<E extends Comparable<E>> {
 
     public E insert(E data, Node<E> tempRoot) {
         if (tempRoot == null) {
-
+            tempRoot = new Node<>(data);
         }
     }
 }
