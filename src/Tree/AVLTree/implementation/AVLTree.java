2fb9c30cb30deecdc804d4b6d70187563e7041f5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-22 10:54:32 +0530

    Add a if() condition inside the if() condition

--- src/Tree/AVLTree/implementation/AVLTree.java
+++ src/Tree/AVLTree/implementation/AVLTree.java
@@ -33,7 +33,9 @@ public class AVLTree<E extends Comparable<E>> {
         } else {
             tempRoot.setRight(insert(data, tempRoot.getRight()));
             if (calculateHeight(tempRoot.getRight()) - calculateHeight(tempRoot.getLeft()) == 2) {
+                if (data.compareTo(tempRoot.getRight().getData()) < 0) {
 
+                }
             }
         }
     }
