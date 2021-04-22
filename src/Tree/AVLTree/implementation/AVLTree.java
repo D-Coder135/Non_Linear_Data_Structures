8e3d7964546539a80001a5500841731a3c19db03
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-23 00:44:18 +0530

    Fix an error!

--- src/Tree/AVLTree/implementation/AVLTree.java
+++ src/Tree/AVLTree/implementation/AVLTree.java
@@ -37,7 +37,7 @@ public class AVLTree<E extends Comparable<E>> {
         } else {
             tempRoot.setRight(insert(data, tempRoot.getRight()));
             if (calculateHeight(tempRoot.getRight()) - calculateHeight(tempRoot.getLeft()) == 2) {
-                if (data.compareTo(tempRoot.getRight().getData()) < 0) {
+                if (data.compareTo(tempRoot.getRight().getData()) > 0) {
                     tempRoot = leftLeftRotation(tempRoot);
                 } else {
                     tempRoot = doubleRotationWithRightChild(tempRoot);
