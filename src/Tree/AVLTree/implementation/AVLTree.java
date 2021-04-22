31d41ecc00988588f62f14bf08a48a3508293712
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-22 10:00:54 +0530

    Add a class AVLTree.java inside the package implementation

---
+++ src/Tree/AVLTree/implementation/AVLTree.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 22/04/21
+ *   Time: 10:00 AM
+ *   File: AVLTree.java
+ */
+
+package Tree.AVLTree.implementation;
+
+public class AVLTree {
+}
